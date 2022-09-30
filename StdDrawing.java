7e99c40b9aea4f6756432e7bdd2c9d0c58ecc913
Sriparno Ganguly <sriparno@outlook.com>
2022-09-30 22:58:56 +0530

    just some modifications

--- StdDrawing.java
+++ StdDrawing.java
@@ -3,42 +3,5 @@ package StdDrawing;
 import javax.swing.*;
 import java.awt.*;
 
-public class StdDrawing
-{
-    StdDrawing(String args[])
-    {
-        if(args == "dark")
-        {
-            setBackground(Color.white);
-        }
-        else
-        {
-            setBackground(Color.black);
-        }
-    }
 
-    public void paint()
-    {
-
-    }
-
-    public void plotpoint(int x, int y, Graphics G)
-    {
-
-    }
-    public void makeGrid(Graphics G)
-    {
-        
-    }
-}
-
-public class Lines
-{
-
-}
-
-public class Ellipses
-{
-
-}
 
