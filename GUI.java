59179a35fc18a020333802ecd713f9fb7770f4ec
Sriparno Ganguly <sriparno@outlook.com>
2022-10-01 20:46:47 +0530

    somethings

---
+++ GUI.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GUI{
+    
+    public GUI(){
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+
+        panel.setBorder(BorderFactory.createEmptyBorder(300,300,50,30));
+        panel.setLayout(new GridLayout(0,1));
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Testing GUI");
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String args[])
+    {
+        new GUI();
+    }
+}
