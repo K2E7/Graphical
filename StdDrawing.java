124edc991921b8ca3431760f88735f95d2ffc980
Sriparno Ganguly <sriparno@outlook.com>
2022-10-01 06:40:35 +0530

    created a new window using swing

--- StdDrawing.java
+++ StdDrawing.java
@@ -1,7 +1,24 @@
-package StdDrawing;
-
 import javax.swing.*;
 import java.awt.*;
 
+public class GUI{
+    
+    public GUI(){
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
 
+        panel.setBorder(BorderFactory.createEmptyBorder(300,300,10,30));
+        panel.setLayout(new GridLayout(0,1));
 
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Testing GUI");
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String args[])
+    {
+        new StdDrawing();
+    }
+}
